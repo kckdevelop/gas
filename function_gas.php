10a7aed4f26433010555e1a9af49adee3ae7427b
kckdevelop <67529415+kckdevelop@users.noreply.github.com>
2020-07-01 18:15:36 +0700

    Update function_gas.php

--- function_gas.php
+++ function_gas.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.52.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -7.8074211,110.3645445";
+$header[] = "X-Location: -7.8117222,110.3595332";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
